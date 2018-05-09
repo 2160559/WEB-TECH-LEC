ff33f45f9e2596e1869655431c5f21c1a51aa32e
2161478 <2161478@slu.edu.ph>
2018-05-09 17:08:00 +0800

    updated navbaar.blade.php

--- finals/finals/resources/views/layouts/with-navbar.blade.php
+++ finals/finals/resources/views/layouts/with-navbar.blade.php
@@ -20,7 +20,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky-top">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <h1><img src="{{asset('storage/img/logo.PNG')}}" alt="logo" style="height: 50px">{{ config('app.name',
